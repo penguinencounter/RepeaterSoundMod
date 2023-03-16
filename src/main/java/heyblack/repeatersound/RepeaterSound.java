ce294141ce7e1b65835d95df7b94c6a4d985f427
HeyBlack233 <88659388+HeyBlack233@users.noreply.github.com>
2023-03-16 14:43:57 +0800

    Add files via upload

--- src/main/java/heyblack/repeatersound/RepeaterSound.java
+++ src/main/java/heyblack/repeatersound/RepeaterSound.java
@@ -5,7 +5,8 @@ import com.mojang.brigadier.arguments.FloatArgumentType;
 import heyblack.repeatersound.config.Config;
 import heyblack.repeatersound.config.ConfigManager;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
@@ -16,10 +17,10 @@ public class RepeaterSound implements ClientModInitializer
     public static final Logger logger = LogManager.getLogger();
 
     public static final Identifier REPEATER_CLICK = new Identifier("repeatersound:repeater_click");
-    public static SoundEvent BLOCK_REPEATER_CLICK = new SoundEvent(REPEATER_CLICK);
+    public static SoundEvent BLOCK_REPEATER_CLICK = SoundEvent.of(REPEATER_CLICK);
 
     public static final Identifier REDSTONE_WIRE_CLICK = new Identifier("repeatersound:redstone_wire_click");
-    public static SoundEvent BLOCK_REDSTONE_WIRE_CLICK = new SoundEvent(REDSTONE_WIRE_CLICK);
+    public static SoundEvent BLOCK_REDSTONE_WIRE_CLICK = SoundEvent.of(REDSTONE_WIRE_CLICK);
 
     ConfigManager cfgManager = ConfigManager.getInstance();
 
@@ -28,13 +29,13 @@ public class RepeaterSound implements ClientModInitializer
     {
         cfgManager.loadConfig();
 
-        ClientCommandManager.DISPATCHER.register(ClientCommandManager.literal("repeatersound")
-                        .then(ClientCommandManager.literal("setBasePitch")
-                                .then(ClientCommandManager.argument("pitch", FloatArgumentType.floatArg())
-                                        .executes(ctx -> saveConfigPitch(FloatArgumentType.getFloat(ctx, "pitch")))))
-                        .then(ClientCommandManager.literal("useRandomPitch")
-                                .then(ClientCommandManager.argument("useRandom", BoolArgumentType.bool())
-                                        .executes(ctx -> saveConfigRandom(BoolArgumentType.getBool(ctx, "useRandom"))))));
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, environment) -> dispatcher.register(ClientCommandManager.literal("repeatersound")
+                .then(ClientCommandManager.literal("setBasePitch")
+                        .then(ClientCommandManager.argument("pitch", FloatArgumentType.floatArg())
+                                .executes(ctx -> saveConfigPitch(FloatArgumentType.getFloat(ctx, "pitch")))))
+                .then(ClientCommandManager.literal("useRandomPitch")
+                        .then(ClientCommandManager.argument("useRandom", BoolArgumentType.bool())
+                                .executes(ctx -> saveConfigRandom(BoolArgumentType.getBool(ctx, "useRandom")))))));
     }
 
     public int saveConfigPitch(float pitch)
